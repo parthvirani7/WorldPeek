0404298dbba3c445bcb498e84392bed4be0d57b9
VPARTH14 <parthscam14@gmail.com>
2025-05-10 20:22:35 +0530

    commit

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -11,17 +11,18 @@ interface CountryCardProps {
 const CountryCard = ({ name, flagUrl, countrySlug, className = '' }: CountryCardProps) => {
   return (
     <Link to={`/country/${countrySlug}`} className={`block ${className}`}>
-      <div className="bg-white rounded-2xl shadow-lg overflow-hidden">
+      <div className="bg-[#f8f8f8] rounded-2xl shadow-lg overflow-hidden">
         {/* Flag Image */}
         <img
           src={flagUrl}
           alt={`${name} Flag`}
           className="w-full h-32 object-cover rounded-t-2xl"
         />
+        </div>
         <div className="p-4">
           <p className="font-semibold text-lg text-center">{name}</p>
         </div>
-      </div>
+      
     </Link>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,11 +15,11 @@ const HomePage = () => {
   return (
     <div className="bg-gray-100 p-8">
         
-      <div className="grid grid-cols-6 grid-rows-1 gap-4 sm:gap-2 md:gap-4 lg:gap-6 xl:gap-8">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7   grid-rows-1 gap-4 sm:gap-2 md:gap-4 lg:gap-6 xl:gap-8">
         {countries.map((country) => (
           <div
             key={country.slug}
-            className="bg-white border border-gray-200 rounded-lg shadow-lg p-4 hover:shadow-2xl transition duration-300"
+            className="bg-white border border-gray-200 rounded-t-3xl rounded-b-xl shadow-lg p-2 hover:shadow-2xl transition duration-300"
           >
             <CountryCard
               name={country.name}
